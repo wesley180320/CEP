90ef1210f62277c7540ce5af8902f9fdf700ae5c
wesley180320 <wesleyestevao333@gmail.com>
2023-06-28 22:57:09 -0400

    Atualização 01

--- cep.js
+++ cep.js
@@ -8,15 +8,13 @@ const bairro = document.getElementById("bairro");
 
 async function pesquisarCep() {
     const cep_valor = cep.value;
-
-    if (cep_valor.length == 8) {
+    if (cepInvalido(cep_valor)) {
         const viacep_url = `https://viacep.com.br/ws/${cep_valor}/json/`;
         const cep_dados = await fetch(viacep_url);
         const cep_json = await cep_dados.json();
-
         console.log(cep_json);
-
         inserirDados(cep_json);
+        redirencionarGit()
     }
 }
 
@@ -31,6 +29,23 @@ async function inserirDados(cep_json) {
     localidade.innerHTML = "Localidade: " + cep_json.localidade;
     logradouro.innerHTML = "Rua: " + cep_json.logradouro;
     bairro.innerHTML = "Bairro: " + cep_json.bairro;
+
+}
+
+function redirencionarGit() {
+    let valor = confirm("Gostaria de Acessar o GitHUb do desenvolvedor?")
+    if (valor) {
+        window.location = "https://github.com/wesley180320?tab=repositories"
+    }
+}
+
+function cepInvalido(cep) {
+    if (cep.length == 8) {
+        return true
+    } else {
+        confirm("Cep Invalido")
+        document.getElementById("cep").value = "";
+    }
 }
 
 pesquisar.addEventListener("click", pesquisarCep);
